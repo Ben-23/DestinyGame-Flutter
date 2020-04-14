e111139bc6013901b455d60d3d6b58d513dbf9d1
Ben Nicholas <ben233.techtopus@users.noreply.github.com>
2020-04-14 22:47:59 +0530

    final commit

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  Story(String st, String one, String two) {
+    storyTitle = st;
+    choice1 = one;
+    choice2 = two;
+  }
+}
